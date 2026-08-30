b3e5a36bb0d24753fedc2e273b9c2d757a423f25
Golam Zaid <golamzaid101@gmail.com>
2026-08-30 19:10:49 +0530

    Fix CORS configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173", 
-        "https://universal-health-record.vercel.app/"
+        "https://universal-health-record.vercel.app"
     ], 
     allow_credentials=True,
     allow_methods=["*"],
